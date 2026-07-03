8bda1dfcaa8388b8bb0bdc053680fb47a4d3faa7
emwangi <ephantusmkamau@gmail.com>
2026-07-03 11:13:50 +0300

    Click-to-reveal on your banner

--- submission/js/scripts.js
+++ submission/js/scripts.js
@@ -97,3 +97,14 @@ InfForm.addEventListener("submit", event => {
         email.value = "";
     }
 })
+
+const bannerImg = document.getElementById('bannerimg');
+const bannerText = document.getElementById('bannertext');
+
+
+bannerImg.addEventListener('click', function() {
+  
+  
+  bannerText.classList.toggle('show');
+  
+});
